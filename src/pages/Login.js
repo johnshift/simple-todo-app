c0abe4c3578b2b4753b7b7079b475a7ec610fe05
John Ballesteros <johnshift.dev@gmail.com>
2021-09-08 21:41:50 +0800

    state hook

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,7 +9,12 @@ import {
   Button,
 } from "@chakra-ui/react";
 
+import { useState } from "react";
+
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <Flex align="center" justify="center" h="100vh">
       <Spacer />
@@ -28,7 +33,13 @@ const Login = () => {
 
           <Center mb={[5, 50]}>
             <FormControl>
-              <Input placeholder="Username" textAlign="center" h={[12, 20]} />
+              <Input
+                placeholder="Username"
+                textAlign="center"
+                h={[12, 20]}
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
             </FormControl>
           </Center>
           <Center mb={[5, 50]}>
@@ -38,12 +49,14 @@ const Login = () => {
                 placeholder="Password"
                 textAlign="center"
                 type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </FormControl>
           </Center>
 
           <Center>
-            <Button w="100%" h={[12, 20]}>
+            <Button w="100%" h={[12, 20]} colorScheme="purple">
               Login
             </Button>
           </Center>
