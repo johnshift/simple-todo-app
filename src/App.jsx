6c03444b0603994b8a231b537fbf2c3670bc8f36
John Ballesteros <johnshift.dev@gmail.com>
2021-09-09 20:26:20 +0800

    add authenticated route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/prop-types */
+
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Switch, Route, Redirect,
+} from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
 
 import Navbar from './components/Navbar';
+
+const UserAuthRoute = ({ children, ...rest }) => {
+  const user = useSelector((state) => state.user.value);
+  const isAuthenticated = user.username === 'johnshift';
+
+  return (
+    <Route
+      {...rest}
+      render={({ location }) => (isAuthenticated
+        ? (children)
+        : (<Redirect to={{ pathname: '/login', state: { from: location } }} />))}
+    />
+  );
+};
 
 function App() {
   return (
@@ -12,9 +32,9 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route exact path="/">
+          <UserAuthRoute exact path="/">
             <Home />
-          </Route>
+          </UserAuthRoute>
           <Route path="/login">
             <Login />
           </Route>
