f758a9c2d899a8b7071b9c0d754f8cef2cf32555
John Ballesteros <johnshift.dev@gmail.com>
2021-09-11 14:05:26 +0800

    fix non-serializable errors

--- src/features/todo.js
+++ src/features/todo.js
@@ -4,13 +4,13 @@ const initState = {
   lastID: 2,
   todoList: [
     {
-      id: 0, description: 'Todo item #1', done: false, due: new Date(),
+      id: 0, description: 'Todo item #1', done: false, due: new Date().toString(),
     },
     {
-      id: 1, description: 'Todo item #2', done: false, due: new Date(),
+      id: 1, description: 'Todo item #2', done: false, due: new Date().toString(),
     },
     {
-      id: 2, description: 'Todo item #3', done: false, due: new Date(),
+      id: 2, description: 'Todo item #3', done: false, due: new Date().toString(),
     },
   ],
 };
@@ -19,23 +19,19 @@ export const todoSlice = createSlice({
   name: 'todo',
   initialState: initState,
   reducers: {
-    addTodo: (state, action) => ({
-      ...state,
-      lastID: state.lastID + 1,
-      todoList: [
-        ...state.todoList,
-        {
-          id: state.lastID + 1,
-          description: action.payload,
-          done: false,
-          due: new Date(),
-        },
-      ],
-    }),
-    deleteTodo: (state, action) => ({
-      ...state,
-      todoList: state.todoList.filter((todo) => todo.id !== action.payload.id),
-    }),
+    addTodo: (state, { payload }) => {
+      const newID = state.lastID + 1;
+      state.lastID = newID;
+      state.todoList.push({
+        id: newID,
+        description: payload,
+        done: false,
+        due: new Date().toString(),
+      });
+    },
+    deleteTodo: (state, { payload }) => {
+      state.todoList = state.todoList.filter((todo) => todo.id !== payload.id);
+    },
   },
 });
 
